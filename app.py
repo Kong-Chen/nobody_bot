0dc408265568243d2e7107a9c2c9fe06612164b6
Kong-Chen <51734766+Kong-Chen@users.noreply.github.com>
2024-06-17 10:34:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import requests
 import re
 from datetime import datetime, time, timedelta
 #import pytz 
-from urllib.parse import quote
+
 
 
 
@@ -163,30 +163,7 @@ def handle_message(event):
         if user_message =='Nasa':
             # API 密鑰
             api_key = "74K2SccksUYY9UL8P6FPb7oz3Vn0JFacjP5ZPdPh"
-
-            # API 網址
-            url = "https://api.nasa.gov/planetary/apod"
-
-            # API 參數
-            params = {
-                "api_key": api_key
-            }
-
-            # 發送 API 請求
-            response = requests.get(url, params=params)
-
-            # 取得 API 的返回值
-            data = response.json()
-
-            # 顯示照片的網址
-            print("Picture URL:", data["url"])
-            line_bot_api.reply_message(
-            event.reply_token,
-            ImageSendMessage(
-                original_content_url=data["url"],
-                preview_image_url=data["url"]
-            )
-            )
+            print (api_key)
             
         elif re.match(r'已經在\d{2}:\d{2}上廁所', user_message):
             # user_message = '已經在15:45上廁所'
